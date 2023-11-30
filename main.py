f7377b16f8d9aa9dd4cc46e0bae9cd58c2e7e905
TaiMann2121 <tai.mann0521@gmail.com>
2023-11-29 23:35:27 -0500

    main function piece
    Initializes 2 player objects with attributues pieces(dictionaries mapping each piece to their respective row, col on board). draws pieces to board in redraAll using the drawPieces function.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cmu_graphics import *
 from menu import *
 from board import *
 from gameScreenIcons import *
+from players import Player
 from PIL import Image
 
 def onAppStart(app):
@@ -20,6 +21,22 @@ def onAppStart(app):
     app.whiteQueen = CMUImage(Image.open('White_Queen.png'))
     app.whiteKing = CMUImage(Image.open('White_King.png'))
 
+    # player 1 -> white pieces
+    app.whitePieces = {app.whitePawn: [(6,0),(6,1),(6,2),(6,3,),(6,4),(6,5),(6,6),(6,7)],
+                       app.whiteRook: [(7,0), (7,7)],
+                       app.whiteKnight: [(7,1),(7,6)],
+                       app.whiteBishop: [(7,2),(7,5)],
+                       app.whiteQueen: [(7,3)], app.whiteKing: [(7,4)]}
+    app.player1 = Player(app.whitePieces)
+
+    # player 2 -> black pieces
+    app.blackPieces = {app.blackPawn: [(1,0),(1,1),(1,2),(1,3,),(1,4),(1,5),(1,6),(1,7)],
+                       app.blackRook: [(0,0), (0,7)],
+                       app.blackKnight: [(0,1),(0,6)],
+                       app.blackBishop: [(0,2),(0,5)],
+                       app.blackQueen: [(0,3)], app.blackKing: [(0,4)]}
+    app.player2 = Player(app.blackPieces)
+
     app.showMenu = True
     app.gameMode = None
     app.board = Board()
@@ -36,18 +53,23 @@ def redrawAll(app):
         #draws board
         drawBoard(app.board)
         drawBorder(app, app.board)
-        # menu Icon
+        # Icons
         drawIcons()
-        drawImage(app.whiteKing, 200, 200, align = 'center')
+        # draws pieces
+        drawPieces(app.board, app.player1)
+        drawPieces(app.board, app.player2)
+
 
 # mousePress function
 def onMousePress(app, mouseX, mouseY):
     # if we are in the menu
     if app.showMenu:
         if mouseX <= 300 and mouseX >= 100:
+            # two player
             if mouseY <= 225 and mouseY >= 125:
                 app.gameMode = 'Two'
                 app.showMenu = False
+            # single player
             elif mouseY <= 350 and mouseY >= 250:
                 app.gameMode = 'Single'
                 app.showMenu = False
