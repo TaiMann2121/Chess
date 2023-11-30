dffa2dc493cafbbda79f850ecea39a4deac69e51
TaiMann2121 <tai.mann0521@gmail.com>
2023-11-29 21:46:01 -0500

    Game screen icons (menu, players)

---
+++ gameScreenIcons.py
@@ -0,0 +1,10 @@
+from cmu_graphics import *
+
+def drawIcons():
+    # draws menu button
+    drawLabel("Press 'm' to go back to menu", 2, 6, align = 'left', size = 12, fill = 'black')
+    # draw player 1 label
+    drawLabel('Player 1', 25, 360, align = 'left', size = 16, fill = 'white')
+    # draw Player 2 Label
+    drawLabel('Player 2', 25, 90, align = 'left', size = 16, fill = 'black')
+
