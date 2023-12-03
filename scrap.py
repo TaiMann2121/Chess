2182eb5c69bb597f666ec8e3b272a08dba819301
TaiMann2121 <tai.mann0521@gmail.com>
2023-12-02 20:28:24 -0500

    ignore

---
+++ scrap.py
@@ -0,0 +1,17 @@
+# import math
+
+# class Board:
+#     def __init__(self):
+#         self.rows, self.cols = 8, 8
+#         self.boardLeft, self.boardTop = 25, 100
+#         self.boardWidth, self.boardHeight = 250, 250
+
+# def getSquare(board, mouseX, mouseY):
+#     squareWidth, squareHeight = 31.25, 31.25
+#     row = math.floor((mouseY - board.boardTop) / squareHeight)
+#     col = math.floor((mouseX - board.boardLeft) / squareWidth)
+#     return row, col
+# board = Board()
+# print(getSquare(board, 26, 150))
+L = [1,2,3,4]
+print('hi' == 'hi')
